d5b8f201654dc6b4b11c12865d9d28fdb697c642
Conner Evans <conner.evans5705@gmail.com>
2024-02-07 23:55:19 +0000

    Level Updates
    Redid levels 1/2 and added 3rd level. Changed level transition from flag to a door

--- Scenes/Levels/level_base.gd
+++ Scenes/Levels/level_base.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 @onready var audio_background = $BackgroundMusic
+@onready var player = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	audio_background.play()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
